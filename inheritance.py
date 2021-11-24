71eb4385993cac6e3025e24bc6c52f80f0e59d66
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-11-24 15:11:49 -0700

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+#List of people invited
+class Invited:
+    name = ''
+    email = ''
+    age = ''
+#kids coming
+class Children:
+    number_kids = ''
+    Boy = ''
+    Girl = ''
+#what they will bring
+class Adults:
+    Food_bringing = ''
+    Drinks_bringing = ''
