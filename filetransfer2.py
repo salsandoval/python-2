294afea307f8c6038adabefddf0869fdea455438
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-12-08 08:43:43 -0700

    updated filetransfer2

--- filetransfer2.py
+++ filetransfer2.py
@@ -3,6 +3,8 @@ from tkinter import *
 import shutil
 import os
 
+
+
 #set where the source of the files are
 source = '/Users/guapo/Desktop/hold/'
 
@@ -13,7 +15,6 @@ files = os.listdir(source)
 for i in files:
     #we are saying move the files represented by 'i' to their new destination
     shutil.move(source+i, destination)
-
 
 root = tk.Tk()
 root.withdraw()
@@ -34,3 +35,31 @@ input('press any key to exit')
 src_path = r"C:\Users\guapo\Desktop\receive"
 dst_path = r"C:\Users\guapo\Desktop\hold"
 shutil.move(src_path, dst_path)
+
+def pickSourceDir():
+    myDir = tKinter.filedialog.askdirectory()
+    source_dir.delete(0, END)
+    source_dir.insert(0, myDir)
+
+def pickDestDir():
+    myDir = tKinter.filedialog.askdirectory()
+    destination_dir.delete(0, END)
+    destination_dir.insert(0, myDir)
+
+def moveFiles():
+    source = source_dir.get()
+    destination = destination_dir.get()
+    source_files = os.listdir(source)
+    for i in source_files:
+        file_path = os.path.join(source, i)
+        hours_ago_24 = datetime.datetime.now() - timedelta(hours = 24)
+        modification_time = os.path.getmtime(file_path)
+        date_time_ofTfile = datetime.datetime.fromtimestamp(modification_time)
+        if hoirs_ago_24 < date_time_of_file:
+            shutil.move(source + '/' + i, destination)
+            print(i + ' was succesfully transferred.')
+
+
+
+if __name__ == '__main__':
+    
